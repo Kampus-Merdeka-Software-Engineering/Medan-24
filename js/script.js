818cd71c6581c542cde1a8bcc6f41b6c19ecd8dd
Muhammad Daffa Fachreza <daffafachreza20@gmail.com>
2024-05-05 11:31:44 +0700

    Editing navbar

--- js/script.js
+++ js/script.js
@@ -39,8 +39,7 @@ document.addEventListener('touchmove', function(evt) {
 });
 
 // Toogle Class Active
-const navbarNav = document.querySelector('.navbar-nav');
-// ketika humberger menu di klik
-document.querySelector('#hamburger-menu').onClick = () => {
-    navbarNav.classList.toggle('active');
-};
+function toggleMenu() {
+  var navbarNav = document.querySelector('.navbar-nav');
+  navbarNav.classList.toggle('show');
+}
