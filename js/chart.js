337477612d7ecbbc77f08ae1f3d17f6ddd6c2c67
azhrkhanayaa <khanayafirdausya10@gmail.com>
2024-05-21 23:38:32 +0700

    adding filter line chart

--- js/chart.js
+++ js/chart.js
@@ -40,6 +40,38 @@ document.addEventListener("DOMContentLoaded", function () {
         },
       });
 
+      document.getElementById('filter-button').addEventListener('click', function() {
+        updateChart(chart, data);
+      });
+      
+      function updateChart(chart, data) {
+        var startDate = new Date(document.getElementById('start-date').value);
+        var endDate = new Date(document.getElementById('end-date').value);
+      
+        if (isNaN(startDate) || isNaN(endDate)) {
+          alert("Please select valid start and end dates.");
+          return;
+        }
+      
+        var filteredData = data.monthly.filter(function (elem) {
+          var elemDate = new Date(elem.month + '-01'); 
+          return elemDate >= startDate && elemDate <= endDate;
+        });
+      
+        var filteredMonths = filteredData.map(function (elem) {
+          return elem.month;
+        });
+      
+        var filteredRevenue = filteredData.map(function (elem) {
+          return elem.total_revenue;
+        });
+      
+        chart.data.labels = filteredMonths;
+        chart.data.datasets[0].data = filteredRevenue;
+        chart.update();
+      }
+      
     }
   };
 });
+
